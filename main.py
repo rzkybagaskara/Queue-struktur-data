90968ffe0a785888200a4d8020f2d9b2fb221453
Rizky Bagaskara <rizky.6abu@hotmail.com>
2021-11-22 21:05:26 +0700

    Benerin layout  menu, spacing, dan penambahan message di menu keluar (5)

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ def create():
   global size
   global tampilan
   size = int(input("Ukuran Queue: "))
-  queue = []
-  tampilan = Q = ["_" for i in range(size)] 
-  print("Queue telah dibuat") 
-  print(tampilan)
+  if size == 0:
+    print("Queue tidak boleh memiliki ukuran 0!\n")
+  else:
+    queue = []
+    tampilan =  ["_" for i in range(size)] 
+    print("Queue telah dibuat") 
+    print("Q =",tampilan)
+    print("FRONT(Q) = NULL")#Karena masih hampa, front = null
+    print("REAR(Q)  = NULL") #karena masih hampa, rear = null
+    print(f"NOEL(Q)  = {len(queue)}\n")#Ngitung panjang queue
   
 
 def insert(q):
@@ -27,21 +33,20 @@ def insert(q):
   global indeks
   global noel
   if len(queue) == size:
-    print("OVERFLOW")
+    print("OVERFLOW\n")
   else:
     queue.append([q, indeks+1])
     tampilan[indeks] = q
     indeks += 1
-    print(tampilan)
+    print("Q =",tampilan)
     # [a,1] = a,1 = 3
     # [1:4] 1 sampe 4
     # a,1
     # print(f"FRONT(Q) = {((queue[0]))[1:4]}")
     rear = len(queue)-1 #Dikurang 1 supaya hasilnya sesuai dengan queuenya (list)
     print(f"FRONT(Q) = {queue[0][0]},{queue[0][1]}")#Front (A) tetap 0, terus front(elemen) indeksnya dimulai dari 1
-    print(f"REAR(Q) = {queue[rear][0]},{queue[rear][1]}")#Rear (A) berkurang 1, terus rear(elemen) indeksnya  1
-    print(f"NOEL(Q) = {len(queue)}")#Ngitung panjang queue
-    print("\n")
+    print(f"REAR(Q)  = {queue[rear][0]},{queue[rear][1]}")#Rear (A) berkurang 1, terus rear(elemen) indeksnya  1
+    print(f"NOEL(Q)  = {len(queue)}\n")#Ngitung panjang queue
 
     if (indeks == size):
         indeks = 0
@@ -55,7 +60,7 @@ def remove():
   global noel
   global tampilan
   if len(queue) == 0:
-    print("UNDERFLOW")
+    print("UNDERFLOW\n")
   else:
     queue.pop(0)
     rear -= 1
@@ -63,16 +68,15 @@ def remove():
     front += 1 
     if front == size:
         front = 0
-    print(tampilan) 
-    #Kondisi array sirkular (remove)
+    print("Q =",tampilan) 
     if len(queue) == 0:
         print(f"FRONT(Q) = NULL")
-        print(f"REAR(Q) = NULL")
-        print(f"NOEL(Q) = {len(queue)}")
+        print(f"REAR(Q)  = NULL")
+        print(f"NOEL(Q)  = {len(queue)}\n")
     else:
         print(f"FRONT(Q) = {queue[0][0]},{queue[0][1]}")#Front (A) tetap 0, terus front(elemen) indeksnya dimulai dari 1
-        print(f"REAR(Q) = {queue[rear][0]},{queue[rear][1]}")#Rear (A) berkurang 1, terus rear(elemen) indeksnya  1
-        print(f"NOEL(Q) = {len(queue)}")#Ngitung panjang queue
+        print(f"REAR(Q)  = {queue[rear][0]},{queue[rear][1]}")#Rear (A) berkurang 1, terus rear(elemen) indeksnya  1
+        print(f"NOEL(Q)  = {len(queue)}\n")#Ngitung panjang queue
   
 
 def isempty(q):
@@ -88,11 +92,11 @@ print('========  QUEUE  =========')
 print('==========================')
 
 while inp_menu == True:
-  print("\n1. CREATE")
-  print("2. INSERT")
-  print("3. REMOVE")
-  print("4. ISEMPTY")
-  print("5. Keluar")
+  print("======MENU======\n|  1. CREATE   |")
+  print("|  2. INSERT   |")
+  print("|  3. REMOVE   |")
+  print("|  4. ISEMPTY  |")
+  print("|  5. KELUAR   |\n================")
   opsi = int(input("Masukkan operasi yang diinginkan: "))
 
   if (opsi == 1) :
@@ -106,3 +110,4 @@ while inp_menu == True:
     isempty(queue)  
   else :
     inp_menu = False  
+    print("Anda keluar dari program...")
